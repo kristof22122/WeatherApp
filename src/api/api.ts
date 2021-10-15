6aecddd62fa0562d0cb1fbaba41c298edc2e147f
kristof22122 <kristf44@gmail.com>
2021-10-15 17:24:32 +0300

    api file changed

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 // export const BASE_URL = 'api.openweathermap.org/data/2.5/weather';
 
 const API_KEY = 'c7b51f444a975bf9f7ef70112646932c';
-const BASE_URL = 'http://api.openweathermap.org/data/2.5/';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
 
 export const request = async (cityName: string, language: string) => {
   const response = await fetch(`${BASE_URL}weather?q=${cityName}&APPID=${API_KEY}&lang=${language}&units=metric`);
